69c0b67d3a20c7b985d70f6c1f49e2f547e80492
dsa9925 <dev.dsa9925@gmail.com>
2024-03-13 13:20:08 +0900

    네이버 지도 적용

--- src/types/map.ts
+++ src/types/map.ts
@@ -2,7 +2,7 @@ import { Coordinates } from './info';
 
 // 네이버 지도 맵에 대한 타입
 export type NaverMap = naver.maps.Map;
-// 네이버 마커에 이미지를 위한 데이터 타입
+// 네이버 마커에 이미지 를 데이터 타입
 export type ImageIcon = {
   url: string;
   size: naver.maps.Size;
@@ -15,4 +15,6 @@ export type Marker = {
   map: NaverMap;
   coordinates: Coordinates;
   icon: ImageIcon;
+  // 사용자가 클릭한 경우 처리
+  onClick: () => void;
 };
